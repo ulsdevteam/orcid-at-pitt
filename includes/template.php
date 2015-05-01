78cdac21c0722b36df015a8fdc72918200109c9e
Clinton Graham <ctgraham@pitt.edu>
2015-05-01 10:57:29 -0400

    cleanup CSS and enabled a responsive design

--- includes/template.php
+++ includes/template.php
@@ -45,17 +45,17 @@
   <div class="foot-col">
     <h2>What is ORCID?</h2>
     <p class="descr">ORCID provides a unique, persistent identifier for researchers—an ID number that can help make your scholarship easier to find and attribute.</p>
-    <p class="linker"><a href="https://orcid.org/">Learn more about ORCID</a></p>
+    <p class="linker"><a class="actionbutton" href="https://orcid.org/">Learn more about ORCID</a></p>
   </div>
   <div class="foot-col">
     <h2>ORCID@Pitt</h2>
     <p class="descr">Find out more about the benefits of an ORCID iD and the university’s effort to encourage Pitt researchers to create an ORCID iD, use it with their scholarship, and connect their ORCID iD with Pitt.</p>
-    <p class="linker"><a href="http://www.library.pitt.edu/orcid">Discover ORCID@Pitt</a></p>
+    <p class="linker"><a class="actionbutton" href="http://www.library.pitt.edu/orcid">Discover ORCID@Pitt</a></p>
   </div>
   <div class="foot-col">
     <h2>Get Help.</h2>
     <p class="descr">If you need help with creating your ORCID iD or have further questions, please contact us.</p>
-    <p class="linker"><a href="mailto:orcidcomm@mail.pitt.edu">orcidcomm@mail.pitt.edu</a></p>
+    <p class="linker"><a class="actionbutton" href="mailto:orcidcomm@mail.pitt.edu">orcidcomm@mail.pitt.edu</a></p>
   </div>
 </footer>
 </div><!-- /end wrapper -->
