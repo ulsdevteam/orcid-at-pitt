f990ffd1c7755c83a9cd65dc4879a1c9c8e4f813
Clinton Graham <ctgraham@pitt.edu>
2015-08-24 08:33:20 -0400

    Remove thank you message from token denial response

--- connect/index.php
+++ connect/index.php
@@ -61,7 +61,7 @@ if (isset($_GET['error'])) {
 			// Ask if the user meant to do that
 			$html = array(
 				'header' => 'ORCID@Pitt Trusted Party Status',
-				'p' => array('Thank you for creating your ORCID iD.', 'You chose not to grant trusted party status to Pitt, thus not allowing the university to access your ORCID iD.', 'Allowing Pitt to be a trusted party will help you and the university maintaining accurate records of your research outputs within Pitt systems such as the Faculty Information System and D-Scholarship@Pitt.', 'If you would like to grant Pitt trusted party status or add information to your ORCID profile, please <a href="/connect">restart this process</a>.', 'To find out more about the ORCID@Pitt initiative and the benefits of having an ORCID iD, please visit the <a href="http://www.library.pitt.edu/orcid">ORCID@Pitt website.</a>'),
+				'p' => array('You chose not to grant trusted party status to Pitt, thus not allowing the university to access your ORCID iD.', 'Allowing Pitt to be a trusted party will help you and the university maintaining accurate records of your research outputs within Pitt systems such as the Faculty Information System and D-Scholarship@Pitt.', 'If you would like to grant Pitt trusted party status or add information to your ORCID profile, please <a href="/connect">restart this process</a>.', 'To find out more about the ORCID@Pitt initiative and the benefits of having an ORCID iD, please visit the <a href="http://www.library.pitt.edu/orcid">ORCID@Pitt website.</a>'),
 			);
 			require('../includes/template.php');
 			exit();
