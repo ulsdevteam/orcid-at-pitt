313fd17bf37eecd7b0fce7fdb84f122654ba634e
Clinton Graham <ctgraham@pitt.edu>
2015-09-10 13:27:22 -0400

    Add Google Analytics

--- includes/template.php
+++ includes/template.php
@@ -11,9 +11,21 @@
 ?><!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="UTF-8" />
+<meta charset="UTF-8" />
 <title>ORCID @ Pitt</title>
 <link href="/styles/default.css" rel="stylesheet" type="text/css" />
+<?php if (ORCID_PRODUCTION): ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-2606773-22', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php endif; ?>
 </head>
 <body>
 <div id="pitt-header" class="blue"><div id="pittlogo"><a id="p-link" title="University of Pittsburgh" href="http://pitt.edu/">University of Pittsburgh</a></div></div>
