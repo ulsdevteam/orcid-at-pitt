57b7b676b2b927ab9644253c7a87bc267cde9553
Clinton Graham <ctgraham@pitt.edu>
2016-08-09 11:13:38 -0400

    Add IE=edge meta tag

--- includes/template.php
+++ includes/template.php
@@ -10,6 +10,7 @@
   }
 ?><!DOCTYPE html>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8" />
 <title>ORCID @ Pitt</title>
