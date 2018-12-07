7939fec4521fec98f11a5c74b65726c1edb383fc
Clinton Graham <ctgraham@pitt.edu>
2018-12-07 10:21:54 -0500

    Document the /find/ endpoint

--- find/index.php
+++ find/index.php
@@ -1,3 +1,5 @@
 <?php
+// This URI will be referenced as an EXTERNAL_WEBHOOK (external-id-url) in the data pushed to orcid.org
+// We may want to do something interesting with it someday; for now, redirect to find.pitt.edu
 header('Location: http://find.pitt.edu/');
 ?>
